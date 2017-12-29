74120536d77734698dafc26b68c40ec4818bd4c5
arielfong <30649040+arielfong@users.noreply.github.com>
2017-12-29 12:06:03 -0800

    edit 1.java

--- day02/1.java
+++ day02/1.java
@@ -2,14 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
-/* Advent of Code 2017, Day 1
- * For each row, determine the difference between the largest value and the smallest value; 
- * the checksum is the sum of all of these differences.
- * 
- * Now, it sounds like the goal is to find the only two numbers in each row where one 
- * evenly divides the other - that is, where the result of the division operation is a whole number. 
- * They would like you to find those numbers on each line, divide them, and add up each line's result.
- */
 
 public class Day2 {
 	public static void main(String[] args) throws FileNotFoundException {
