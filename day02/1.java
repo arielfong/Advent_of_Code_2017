f1169e2f4d91fe8492cb9819eb475bfb6f80d1d6
arielfong <30649040+arielfong@users.noreply.github.com>
2017-12-29 12:12:32 -0800

    edit 1.java

--- day02/1.java
+++ day02/1.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Day2 {
-	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File("input2.txt");
-		Scanner keyboard = new Scanner(file);
-		int checksum = 0;
-		int high;
-		int low;
-		while (keyboard.hasNextLine()) {
-			String[] nums = (keyboard.nextLine()).split("\\s+");
-			int[] intNums = new int[nums.length];
-			for (int i = 0; i < intNums.length; i++) {
-				intNums[i] = Integer.parseInt(nums[i]);
-			}
-			Arrays.sort(intNums);
-			high = intNums[intNums.length-1];
-			low = intNums[0];
-			checksum += high-low;
-		}
-		System.out.println(checksum);
-	}
+  public static void main(String[] args) throws FileNotFoundException {
+    File file = new File("input2.txt");
+    Scanner keyboard = new Scanner(file);
+    int checksum = 0;
+    int high;
+    int low;
+    while (keyboard.hasNextLine()) {
+      String[] nums = (keyboard.nextLine()).split("\\s+");
+      int[] intNums = new int[nums.length];
+      for (int i = 0; i < intNums.length; i++) {
+        intNums[i] = Integer.parseInt(nums[i]);
+      }
+      Arrays.sort(intNums);
+      high = intNums[intNums.length-1];
+      low = intNums[0];
+      checksum += high-low;
+    }
+    System.out.println(checksum);
+  }
 }
